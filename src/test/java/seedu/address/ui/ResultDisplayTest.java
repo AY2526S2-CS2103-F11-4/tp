46c0e829b4c6561f26661944b4e291edbccdf7e5
annamacinnes <anna.macinnes05@gmail.com>
2026-03-17 01:19:28 +0800

    Update exit feature with parser and ui with test class

--- src/test/java/seedu/address/ui/ResultDisplayTest.java
+++ src/test/java/seedu/address/ui/ResultDisplayTest.java
@@ -1,5 +1,9 @@
 package seedu.address.ui;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.concurrent.CountDownLatch;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -8,25 +12,65 @@ import javafx.application.Platform;
 public class ResultDisplayTest {
 
     @BeforeAll
-    public static void initToolkit() {
+    public static void initToolkit() throws InterruptedException {
+        System.setProperty("java.awt.headless", "true");
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+
+        CountDownLatch latch = new CountDownLatch(1);
         try {
-            Platform.startup(() -> {});
+            Platform.startup(latch::countDown);
         } catch (IllegalStateException e) {
-            // toolkit already started
+            latch.countDown();
         }
+        latch.await();
     }
 
     @Test
-    public void setFeedbackToUser_removesExitStyle() {
-        ResultDisplay resultDisplay = new ResultDisplay();
-        resultDisplay.setFeedbackToUser("Hello");
+    public void setFeedbackToUser_normalMessage_success() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Platform.runLater(() -> {
+            ResultDisplay resultDisplay = new ResultDisplay();
+            assertDoesNotThrow(() -> resultDisplay.setFeedbackToUser("Hello"));
+            latch.countDown();
+        });
+        latch.await();
     }
 
     @Test
-    public void setExitMessage_addsExitStyle() {
-        ResultDisplay resultDisplay = new ResultDisplay();
+    public void setFeedbackToUser_afterExitMessage_removesExitStyle() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Platform.runLater(() -> {
+            ResultDisplay resultDisplay = new ResultDisplay();
+            resultDisplay.setExitMessage("Bye");
+            assertDoesNotThrow(() -> resultDisplay.setFeedbackToUser("Hello again"));
+            latch.countDown();
+        });
+        latch.await();
+    }
 
-        resultDisplay.setExitMessage("Bye");
-        resultDisplay.setExitMessage("Bye again");
+    @Test
+    public void setExitMessage_normalMessage_success() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Platform.runLater(() -> {
+            ResultDisplay resultDisplay = new ResultDisplay();
+            assertDoesNotThrow(() -> resultDisplay.setExitMessage("Goodbye!"));
+            latch.countDown();
+        });
+        latch.await();
+    }
+
+    @Test
+    public void setExitMessage_calledTwice_doesNotDuplicateStyle() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Platform.runLater(() -> {
+            ResultDisplay resultDisplay = new ResultDisplay();
+            resultDisplay.setExitMessage("Bye");
+            assertDoesNotThrow(() -> resultDisplay.setExitMessage("Bye again"));
+            latch.countDown();
+        });
+        latch.await();
     }
 }
